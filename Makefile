da2a3a26be3c0c7da3c461609ce832730fcfdaca
David Francos Cuartero <yo.orco@gmail.com>
2011-01-10 08:48:11 +0100

    Renamed CPIM to COPIM

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 install:
-	install -d /usr/share/cpim
-	cp -r conf/* /usr/share/cpim
-	install src/cpim /usr/bin
+	install -d /usr/share/copim
+	cp -r conf/* /usr/share/copim
+	install src/copim /usr/bin
 	install src/*.* /usr/bin
-	mv /usr/bin/cpim.conf /etc
+	mv /usr/bin/copim.conf /etc
 	install src/helpers/* /usr/bin 
--- conf/irssi/scripts/autorun/abook_whois.pl
+++ conf/irssi/scripts/autorun/abook_whois.pl
@@ -3,16 +3,16 @@ use Symbol;
 use vars qw($VERSION %IRSSI); 
 $VERSION = "0.1";
 %IRSSI = (
-    name	=> "Irssi cpim, integrates irssi with cpim, looking for nickname fields in abook .",
+    name	=> "Irssi copim, integrates irssi with copim, looking for nickname fields in abook .",
     description	=> "Show IRC userinfo extracted from a abook-like file in ~/.abook/",
     license	=> "Public Domain",
 );
 
 Irssi::theme_register(['whois_userinfo', '{whois userinfo $1}']);my $mynick;
-sub get_current_nick_data { return `cpim.abook @_ `;}
-sub get_current_nick_mobile { return `cpim.abook @_|cut -d, -f2|cut -d" " -f2 &`; }
-sub call{ system("cpim.call", "+34$_[0]");return;}
-sub kill_skype{system('cpim.stopcall');}
+sub get_current_nick_data { return `copim.abook @_ `;}
+sub get_current_nick_mobile { return `copim.abook @_|cut -d, -f2|cut -d" " -f2 &`; }
+sub call{ system("copim.call", "+34$_[0]");return;}
+sub kill_skype{system('copim.stopcall');}
 sub mail{ system("screen -X at Mutt stuff 'm' && screen -X at Mutt stuff '$_[1]'"); } # FIXME Make this support multiple screen sessions.
 sub make_call{ $fone=&get_current_nick_mobile($_[0]); chomp($fone); print CLIENTCRAP "Calling $fone for $_[0]";  return &call($fone); }
 sub event_whois { my ($server, $data) = @_; my ($temp, $nick) = split(" ", $data); $mynick=$nick; }
